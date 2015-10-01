051378fb6039b06f876ae435c352d500ac26c830
Steven Quiroa <stevenkm92@gmail.com>
2015-10-01 12:11:31 -0600

    Translated to english

--- assets/parse-push.js
+++ assets/parse-push.js
@@ -7,14 +7,14 @@
 
 	function submitForm(evt){
 		evt.preventDefault()
-		console.log('submitForm', this)
+		// console.log('submitForm', this)
 		var xhr = new XMLHttpRequest()
 		var data = new FormData(this)
 		var status = document.getElementById('status')
 		status.style.color = 'inherit'
-		status.innerHTML = 'Cargando...'
+		status.innerHTML = 'Loading...'
 		data.append('security', ajax_object.ajax_nonce)
-		console.log(data)
+		// console.log(data)
 		xhr.open(this.method, ajax_object.ajax_url)
 		xhr.responseType = 'json'
 		xhr.timeout = 15000
@@ -23,20 +23,20 @@
 			if (xhr.readyState != 4) return;
 			if (xhr.status == 200 && xhr.response.result) {
 				status.style.color = 'green'
-				status.innerHTML = 'Tu push ha sido enviada.'
+				status.innerHTML = 'Your push has sent.'
 			} else if(xhr.status == 500 || !xhr.response.result){
 				status.style.color = 'red'
 				status.innerHTML = '<b>Code: </b>' + xhr.response.code + '<br/><b>Message: </b>' + xhr.response.message + '<br/>Search possible error <a href="https://www.parse.com/docs/php/guide#errors">here</a>.'
 			} else {
 				status.style.color = 'red'
-				status.innerHTML = 'Error desconocido'
+				status.innerHTML = 'Unkown Error'
 			}
-			console.log(xhr.response)
+			// console.log(xhr.response)
 		}
 	}
 	function onKeyUp(evt){
 		var self = this
-		console.log('onKeyUp', evt, this.textLength)
+		// console.log('onKeyUp', evt, this.textLength)
 
 		if (this.textLength >= max_length) {
 			this.value = this.value.substr(0, max_length)
@@ -53,7 +53,7 @@
 	var form = document.forms['sendpush']
 
 	//functions
-	console.log(form, form['message'], max_length)
+	// console.log(form, form['message'], max_length)
 	document.getElementById('message_chars').innerHTML = max_length - document.getElementById('message').textLength
 
 	//Events
--- parse-push-wordpress.php
+++ parse-push-wordpress.php
@@ -1,8 +1,8 @@
 <?php
 /*
 	Plugin Name: Parse Push for Wordpress
-	Plugin URI: http://quiroa.me/plugins/parse-push-wordpress/
-	Description: 
+	Plugin URI: http://quiroa.me/parse-push-for-wordpress/
+	Description: Parse Push Wordpress is simple send parse push notifications from wordpress backend for android and ios.
 	Author: Steven Quiroa
 	Version: 1.0.0
 	Author URI: http://quiroa.me
@@ -57,45 +57,46 @@ function register_parse_push(){
 
 function parse_push_sender(){
 	$channels = esc_attr( get_option('pp-channels'));
+	$characters = esc_attr( get_option( 'pp-max-length', 100 ) );
 	if ($channels) {
 		$channels = explode(',', $channels);
 		array_walk($channels, 'trim_value');
 	}
 	?>
 	<div class="wrap">
-		<h2>Send Parse Push</h2>
+		<h2><?php _e( 'Send Parse Push', 'parse-push-wordpress' ); ?></h2>
 		<form action="admin-post.php" name="sendpush" method="POST">
 			<table class="form-table">
 				<tr valign="top">
-		        	<th scope="row">Mensaje</th>
+		        	<th scope="row"><?php _e( 'Message', 'parse-push-wordpress' ); ?></th>
 		        	<td>
 		        	<textarea name="message" id="message" cols="30" rows="3"></textarea>
-		        	<p class="description" id="message_characters">Caracteres Restantes: <span id="message_chars">100</span></p>
+		        	<p class="description" id="message_characters"><?php _e( 'Characters Left', 'parse-push-wordpress' ); ?>: <span id="message_chars"><?php echo $characters; ?></span></p>
 		        	</td>
 		        </tr>
 		        <tr valign="top">
-		        	<th scope="row">Dispositivos</th>
+		        	<th scope="row"><?php _e( 'Devices', 'parse-push-wordpress' ); ?></th>
 		        	<td>
-		        	<fieldset><legend class="screen-reader-text"><span>Dispositivos</span></legend>
+		        	<fieldset><legend class="screen-reader-text"><span><?php _e( 'Devices', 'parse-push-wordpress' ); ?></span></legend>
 					<label for="scope_ios">
 						<input name="scope" id="scope_all" value="all" checked="checked" type="radio">
-						All</label>
+						<?php _e( 'All', 'parse-push-wordpress' ); ?></label>
 					<br>
 					<label for="scope_android">
 						<input name="scope" id="scope_android" value="android" type="radio"> 
-						Android</label>
+						<?php _e( 'Android', 'parse-push-wordpress' ); ?></label>
 					<br>
 					<label for="scope_ios">
 						<input name="scope" id="scope_ios" value="ios" type="radio">
-						iOs</label>
+						<?php _e( 'iOS', 'parse-push-wordpress' ); ?></label>
 					</fieldset>
 		        	</td>
 		        </tr>
 		        <?php if (is_array($channels) and count($channels) > 0): ?>		        	
 		        <tr valign="top">
-		        	<th scope="row">Canales</th>
+		        	<th scope="row"><?php _e( 'Channels', 'parse-push-wordpress' ); ?></th>
 		        	<td>
-		        	<fieldset><legend class="screen-reader-text"><span>Canales</span></legend>
+		        	<fieldset><legend class="screen-reader-text"><span><?php _e( 'Channels', 'parse-push-wordpress' ); ?></span></legend>
 		        	<?php foreach ($channels as $c): ?>
 						<label for="channels_<?php echo $c; ?>"><input name="channels" id="channels_<?php echo $c; ?>" value="<?php echo $c; ?>" type="radio"> <?php echo ucfirst($c); ?></label>
 						<br/>
@@ -116,33 +117,33 @@ function parse_push_sender(){
 
 function parse_push_settings(){?>
 	<div class="wrap">
-		<h2>Parse Push Settings</h2>
+		<h2><?php _e( 'Parse Push Settings', 'parse-push-wordpress' ); ?></h2>
 		<form method="post" action="options.php"> 
 			<?php settings_fields( 'parse-push_settings-group' ); ?>
 			<?php do_settings_sections( 'parse-push_settings-group' ); ?>
 			<table class="form-table">
 		        <tr valign="top">
-		        	<th scope="row">Application ID</th>
+		        	<th scope="row"><?php _e( 'Application ID', 'parse-push-wordpress' ); ?></th>
 		        	<td><input type="text" class="regular-text"name="pp-application_id" value="<?php echo esc_attr( get_option('pp-application_id') ); ?>" /></td>
 		        </tr>
 		        <tr valign="top">
-		        	<th scope="row">Rest API Key</th>
+		        	<th scope="row"><?php _e( 'REST API Key', 'parse-push-wordpress' ); ?></th>
 		        	<td><input type="text" class="regular-text"name="pp-rest_api_key" value="<?php echo esc_attr( get_option('pp-rest_api_key') ); ?>" /></td>
 		        </tr>
 				<tr valign="top">
-		        	<th scope="row">Master Api</th>
+		        	<th scope="row"><?php _e( 'Master Key', 'parse-push-wordpress' ); ?></th>
 		        	<td><input type="text" class="regular-text"name="pp-master_api_key" value="<?php echo esc_attr( get_option('pp-master_api_key') ); ?>" />
 		        	</td>
 		        </tr>
 		        <tr valign="top">
-		        	<th scope="row">Max Length</th>
+		        	<th scope="row"><?php _e( 'Max Length', 'parse-push-wordpress' ); ?></th>
 		        	<td><input type="number" class="regular-int"name="pp-max-length" min="0" value="<?php echo esc_attr( get_option('pp-max-length') ); ?>" />
 		        	</td>
 		        </tr>
 		        <tr valign="top">
-		        	<th scope="row">Channels</th>
+		        	<th scope="row"><?php _e( 'Channels', 'parse-push-wordpress' ); ?></th>
 		        	<td><input type="text" class="regular-text"name="pp-channels" min="0" value="<?php echo esc_attr( get_option('pp-channels') ); ?>" />
-		        	<p class="description" id="message_characters">Coloque los canales literalmente como aparecen en parse y separados por comas. Ej: eudos,pitufo,conejo</p>
+		        	<p class="description" id="message_characters"><?php _e( 'Set channels appear literally as parse and separated by commas. Eg: PHPFans,PythonFans,nodejsfans', 'parse-push-wordpress' ); ?></p>
 		        	</td>
 		        </tr>
 			</table>
